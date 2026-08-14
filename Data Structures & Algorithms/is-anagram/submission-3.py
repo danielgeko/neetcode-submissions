043e015a45197fcebb47a053ead929f5880bc691
danielgeko <danielgekonde@gmail.com>
2026-08-14 15:26:48 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+
+        for ch in s:
+            if ch in s_dict:
+                s_dict[ch]+=1
+            else:
+                s_dict[ch] = 1
+
+        for ch in t:
+            if ch in t_dict:
+                t_dict[ch]+=1
+            else:
+                t_dict[ch] = 1
+
+        return s_dict == t_dict
+        
